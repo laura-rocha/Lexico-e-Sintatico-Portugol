b62573c851c716000385cb71aebb19a18de8fcd2
Laura Rocha <laura13122001@gmail.com>
2022-11-10 09:41:03 -0300

    Correções tratamento de erro

--- src/Sintatico.cpp
+++ src/Sintatico.cpp
@@ -30,7 +30,7 @@ void Sintatico::comparaToken(int i){
 }
 
 void Sintatico::erroToken(){
-	cout << "ERRO SINTÁTICO. Linha: " << lexico.getLinha() << " Coluna: " << (lexico.getColuna() - lexico.getTokenAnterior().size() + 1);
+	cout << "ERRO DE SINTAXE. Linha: " << lexico.getLinha() << " Coluna: " << (lexico.getColuna() - lexico.getTokenAnterior().size() + 1);
 	cout << " -> '" << lexico.getTokenAnterior() << "'";
 	exit(1);
 }
